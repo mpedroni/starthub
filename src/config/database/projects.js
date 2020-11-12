e3da9c5af481e9b49e15673f3bb3489befa7fec9
mpedroni <mpedroni77@gmail.com>
2020-11-12 05:27:10 -0300

    declaração do "banco de dados"

---
+++ src/config/database/projects.js
@@ -0,0 +1,41 @@
+export default [
+  {
+    id: 1,
+    title: "Geração de energia elétrica utilizando garrafas PET e energia solar",
+    name: "PET Energy",
+    author: {
+      id: 1,
+      name: "Eliezer Duarte",
+      skills: ["biólogo", "pesquisador", "professor de biologia"]
+    },
+    description: "Por meio de painéis solares feitos, basicamente, utilizando garrafas PET, eu criei um jeito eficiente e barato de gerar energia elétrica a partir da luz/energia solar",
+    tags: ["energia", "energia limpa", "energia solar", "sustentabilidade", "meio ambiente"],
+    need: ["dev C/C++", "advogado", "engenheiro eletricista"]
+  },
+  {
+    id: 2,
+    title: "Aplicativo mobile de compartilhamento de fotos e vídeos",
+    name: "Instabook",
+    author: {
+      id: 2,
+      name: "Augusto Hoffmann",
+      skills: ["data scientist", "dev python", "processamento de dados", "mysql"]
+    },
+    description: "Aplicativo em formato de rede social onde os usuários cadastrados poderão compartilhar vídeos e fotos com sua rede de amigos (seguidores), podendo estes interagir uns com os outros dentro do aplicativo/rede social",
+    tags: ["app", "mobile", "rede social", "fotos e vídeos"],
+    need: ["dev frontend", "ux designer", "advogado com conhecimento em direitos autorais"]
+  },
+  {
+    id: 3,
+    title: "Software livre de gestão empresarial",
+    name: "Erudion",
+    author: {
+      id: 2,
+      name: "Jhony Filipe Augusto Gonçalves",
+      skills: ["dev php", "dev nodejs", "dev vuejs", "dev angular", "docker"]
+    },
+    description: "Software que dá suporte e permite todo o processo de gestão empresarial, desde o planejamento até projeções futuras",
+    tags: ["gestao empresarial", "php", "vuejs"],
+    need: ["ux designer", "dev frontend", "devops engineer", "postgress DBA"]
+  }
+]
---
+++ src/config/database/users.js
@@ -0,0 +1,41 @@
+export default [
+  {
+    id: 1,
+    name: "Eliezer Duarte",
+    skills: ["biólogo", "pesquisador", "professor de biologia"],
+    locality: {
+      city: "São Paulo",
+      state: "SC"
+    },
+    contact: {
+      github: "https://github.com",
+      linkedin: "https://linkedin.com"
+    }
+  },
+  {
+    id: 2,
+    name: "Augusto Hoffmann",
+    skills: ["data scientist", "dev python", "processamento de dados"],
+    locality: {
+      city: "Salvador",
+      state: "BA"
+    },
+    contact: {
+      github: "https://github.com",
+      linkedin: "https://linkedin.com"
+    }
+  },
+  {
+    id: 3,
+    name: "Jhony Filipe Augusto Gonçalves",
+    skills: ["dev php", "dev nodejs", "dev vuejs", "dev angular", "docker"],
+    locality: {
+      city: "Tarauacá",
+      state: "AC"
+    },
+    contact: {
+      github: "https://github.com",
+      linkedin: "https://linkedin.com"
+    }
+  }
+]
